a7304cd57114936360d2d1fd97b963d4f2ec97ee
Derek Mauildin <derek.e.mauldin@gmail.com>
2016-05-04 15:13:47 -0600

    added ng2 template

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import {bootstrap} from "angular2/platform/browser";
 import {HTTP_PROVIDERS} from "angular2/http";
+import {VarChar2000} from "./varchar2000";
 import "rxjs/Rx";
 
-@Component
+bootstrap(VarChar2000);
--- app/varchar2000-app.ts
+++
@@ -1,3 +0,0 @@
-import {Component} from "angular2/core";
-import {HTTP_PROVIDERS} from "angular2/http";
-
